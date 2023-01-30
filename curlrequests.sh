597c3855bbcf22e908f0ccc8848a43cde1f26898
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-01-30 14:37:38 +0100

    Rename default.conf to custom_setup.conf

---
+++ curlrequests.sh
@@ -0,0 +1,12 @@
+curl http://127.0.0.1/p1/ &
+curl http://127.0.0.1/p1/ &
+curl http://127.0.0.1/p1/ &
+curl http://127.0.0.1/p1/ &
+curl http://127.0.0.1/p1/ &
+curl http://127.0.0.1/p1/ &
+curl http://127.0.0.1/p1/ &
+curl http://127.0.0.1/p1/ &
+curl http://127.0.0.1/p1/ &
+curl http://127.0.0.1/p1/ &
+curl http://127.0.0.1/p1/ &
+curl http://127.0.0.1/p1/ &
